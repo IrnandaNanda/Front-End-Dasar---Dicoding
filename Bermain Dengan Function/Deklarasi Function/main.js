061c2da8f7ce07f08c7dd3e8edcd296652087d18
IrnandaNanda <irnandatri1509@gmail.com>
2025-02-21 10:22:46 +0700

    Deklarasi Function

---
+++ Bermain Dengan Function/Deklarasi Function/main.js
@@ -0,0 +1,5 @@
+function convertCelsiusToFahrenheit(temperature) {
+    const temperatureInFahrenheit = 9 / 5 * temperature + 32;
+   
+    console.log('Hasil konversi:', temperatureInFahrenheit);
+  }
